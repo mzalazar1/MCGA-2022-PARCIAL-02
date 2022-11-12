671bda027548c2f73c18e810c51db43f3cdfc739
mzalazar1 <marcoszalazar86@gmail.com>
2022-11-11 23:19:43 -0300

    MCGA-PARCIAL-002

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <p>Eliminar esta etiqueta</p>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
