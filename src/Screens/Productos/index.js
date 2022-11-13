521209f20030e7f7f8e26cc902f0277faa163db9
pabloatoledo <103152688+pabloatoledo@users.noreply.github.com>
2022-11-12 21:32:04 -0300

    MCGA-p08:Edita botones compartidos

--- src/Screens/Productos/index.js
+++ src/Screens/Productos/index.js
@@ -3,28 +3,45 @@ import HeaderSection from '../../Components/Header/Header';
 
 import FooterSection from '../../Components/Footer/Footer';
 import Boton from "../../Components/SharedComponents/Boton";
+import styles from './Index.module.css'
 let linkProd = 'http://localhost:4000/exam_01_mcga/products/all';
 let productos = '';
 
-const obtProductos = () => {
-    fetch (linkProd)
-    .then(function(respuesta) {
-        //return respuesta.json()
-        productos = respuesta.json();
-    })
-}
+// const obtProductos = () => {
+//     fetch (linkProd)
+//     .then(function(respuesta) {
+//         //return respuesta.json()
+//         productos = respuesta.json();
+//     })
+// }
 
-const Recursos = () => {
+const Productos = () => {
     return(
         <div>
             <HeaderSection />
-            <Boton
-            tipo='prodABM'
-            texto='Obtener todos' />
+            <div className={styles.botProd}>
+                <Boton
+                tipo='prodABM'
+                texto='Obtener todos' />
+                <Boton
+                tipo='prodABM'
+                texto='Agregar nuevo' />
+                <Boton
+                tipo='prodABM'
+                texto='Modificar' />
+                <Boton
+                tipo='prodABM'
+                texto='Eliminar' />
+            </div>
+            
             <h1>Esta es la parte de productos</h1>
+
+            Tabla
+
+
             <FooterSection />
         </div>
     );
 }
 
-export default Recursos;
+export default Productos;
