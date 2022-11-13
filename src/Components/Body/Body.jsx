513e187a086e819d6b4b984216641b50e655b934
pabloatoledo <103152688+pabloatoledo@users.noreply.github.com>
2022-11-13 19:54:42 -0300

    MCGA-P13: inicio tabla productos

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -4,7 +4,7 @@ import styles from './Body.module.css';
 const BodySection = () => {
     return (
         <div className={styles.cuerpo}>
-            <h1 className={styles.h1}>Bienvenido a *Nombre aplicación*</h1>
+            <h1 className={styles.h1}>Bienvenido a Aplicación Parcial MCGA</h1>
             <h2 className={styles.h2}>Esta aplicación fue desarrollada por Marcos Zalazar y Pablo Toledo para rendir el segundo parcial de la materia MCGA en 2022</h2>
             <p className={styles.texto}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum nobis ullam laudantium tempora unde magni qui accusantium similique obcaecati ea nemo, nam, ducimus alias eveniet quidem sint rem ex sunt.</p>
             <p className={styles.texto}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim officiis ducimus eaque in dolores quam. Quos quis labore quia. Mollitia deleniti fugit quo quae necessitatibus quam animi odit maxime soluta.</p>
--- src/Screens/Productos/index.js
+++ src/Screens/Productos/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import HeaderSection from '../../Components/Header/Header';
-
 import FooterSection from '../../Components/Footer/Footer';
 import Boton from "../../Components/SharedComponents/Boton";
-import Form from "../../Components/Form/Form";
 import styles from './Index.module.css'
+import Tabla from '../../Components/Table/table'
 let linkProd = 'http://localhost:4000/exam_01_mcga/products/all';
 let productos = '';
 
@@ -14,30 +13,6 @@ const obtProductos = () => {
             //return respuesta.json()
             productos = respuesta.json();
         })
-}
-// const Table = () => {
-//      return(
-//         <div>
-// <Table />
-//         </div>
-//      )
-// }
-const Recursos = () => {
-    return (
-        <div>
-            <HeaderSection />
-            <Boton
-                tipo='prodABM'
-                texto='Obtener todos' />
-            <h1>Esta es la parte de productos</h1>
-            < Form
-                id='001'
-                descripcion='coca cola'
-                stock='20'
-                precio='300'
-            />
-        </div>
-    )
 }
 
 const Productos = () => {
@@ -51,17 +26,9 @@ const Productos = () => {
                 <Boton
                 tipo='prodABM'
                 texto='Agregar nuevo' />
-                <Boton
-                tipo='prodABM'
-                texto='Modificar' />
-                <Boton
-                tipo='prodABM'
-                texto='Eliminar' />
             </div>
-            
-            <h1>Esta es la parte de productos</h1>
 
-            Tabla
+            <Tabla />
 
             <FooterSection />
         </div>
